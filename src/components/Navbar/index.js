f072ba91418abfeb49131de9cdb744c998215a4c
kiran <kiranugale239@gmail.com>
2024-11-22 09:49:29 +0530

    add navbar for mobile

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,10 +1,51 @@
+"use client";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { AlignLeft, MenuIcon } from "lucide-react";
+import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
+
 export default function Navbar() {
+  const router = useRouter();
+
+  const pathname = usePathname();
+  const navItem = [
+    {
+      name: "Home",
+      path: "/",
+      show: true,
+    },
+    {
+      name: "Get Assessment",
+      path: "/assessment",
+      show: true,
+    },
+    {
+      name: "Saved Assessment",
+      path: "/savedAssement",
+      show: true,
+    },
+    {
+      name: "Dashboard",
+      path: "/profile",
+      show: true,
+    },
+  ];
+
   return (
     <>
       <header className="flex items-center justify-between whitespace-nowrap border-b border-solid border-b-[#F0F2F5] px-3 lg:px-10 py-3">
         <div className="flex items-center gap-2 text-[#141414] py-3 px-0 lg:p-auto">
           <div className="size-6">
             <svg
+              className=""
               viewBox="0 0 48 48"
               fill="none"
               xmlns="http://www.w3.org/2000/svg"
@@ -17,36 +58,65 @@ export default function Navbar() {
               />
             </svg>
           </div>
-          <h2 className=" lg:p-auto text-[#141414] text-[25px] font-bold leading-tight tracking-[-0.015em]">
+          <h2 className=" lg:p-auto text-[#141414] text-[25px]  font-bold leading-tight tracking-[-0.015em]">
             Health Check
           </h2>
+          <Sheet>
+            <SheetTrigger
+              className={`flex mt-0  ml-10  lg:hidden ${
+                true ? "block" : "hidden"
+              }`}
+            >
+              <AlignLeft className="font-2xl text-[#80ED99] text-black size-9 mx-10    " />
+            </SheetTrigger>
+
+            <SheetContent className="py-24">
+              <SheetTitle
+                className="uppercase text-2xl semibold text-[#80ED99] "
+                onClick={() => {
+                  router.push("/");
+                }}
+              >
+                Health Check
+              </SheetTitle>
+              <div className=" flex flex-col lg:hidden justify-start  mt-6 gap-5   ">
+                {navItem.map((d) => {
+                  return (
+                    <div key={d.name}>
+                      {d.show ? (
+                        <SheetClose asChild>
+                          <Link
+                            href={d.path}
+                            className="font-semibold uppercase flex min-w-[84px] max-w-[480px] cursor-pointer items-center justify-center overflow-hidden rounded-xl h-10 px-4 bg-[#80ED99] hover:bg-sky-300 bg-[#80ED99]text-[#111418] text-sm font-bold leading-normal tracking-[0.015em] "
+                          >
+                            {d.name}
+                          </Link>
+                        </SheetClose>
+                      ) : null}
+                    </div>
+                  );
+                })}
+              </div>
+              <div
+                className="hidden lg:flexmt-6 ml-10"
+                style={{ display: `${true ? "block" : "none"}` }}
+              ></div>
+            </SheetContent>
+          </Sheet>
         </div>
         <div className="hidden lg:flex flex-1 justify-end gap-8 ">
           <div className="flex items-center gap-9">
-            <a
-              className="text-[#141414] text-sm font-medium leading-normal"
-              href="#"
-            >
-              Dashboard
-            </a>
-            <a
-              className="text-[#141414] text-sm font-medium leading-normal"
-              href="#"
-            >
-              Assessment
-            </a>
-            <a
-              className="text-[#141414] text-sm font-medium leading-normal"
-              href="#"
-            >
-              Education
-            </a>
-            <a
-              className="text-[#141414] text-sm font-medium leading-normal"
-              href="#"
-            >
-              Community
-            </a>
+            {navItem.map((d) => {
+              return (
+                <a
+                  key={d.name}
+                  className="text-[#141414] text-sm font-medium leading-normal"
+                  href="#"
+                >
+                  {d.name}
+                </a>
+              );
+            })}
           </div>
           <div className="flex gap-2">
             <button className="flex min-w-[84px] max-w-[480px] cursor-pointer items-center justify-center overflow-hidden rounded h-10 px-4 bg-[#80ED99] text-[#FFFFFF] text-sm font-bold leading-normal tracking-[0.015em]">
---
+++ src/components/homepage/index.js
@@ -0,0 +1,96 @@
+export default function HomePage() {
+  return (
+    <>
+      <div className="lg:px-0 flex flex-1 justify-center lg:py-0">
+        <div className="layout-content-container flex flex-col  w-full flex-1">
+          <div className="@container">
+            <div className="@[480px]:p-4">
+              <div
+                className="flex min-h-[480px] flex-col gap-6 bg-cover bg-center bg-no-repeat @[480px]:gap-8 @[480px]:rounded items-start justify-end px-4 pb-10 @[480px]:px-10"
+                style={{
+                  backgroundImage:
+                    'linear-gradient(rgba(0, 0, 0, 0.1) 0%, rgba(0, 0, 0, 0.4) 100%), url("be1.jpg")',
+                }}
+              >
+                <div className="flex flex-col gap-2 text-left">
+                  <h1 className="text-white text-4xl font-black leading-tight tracking-[-0.033em] @[480px]:text-5xl @[480px]:font-black @[480px]:leading-tight @[480px]:tracking-[-0.033em]">
+                    Understand your health risks
+                  </h1>
+                  <h2 className="text-white text-sm font-normal leading-normal @[480px]:text-base @[480px]:font-normal @[480px]:leading-normal">
+                    Get personalized insights and recommendations from top
+                    doctors to help you make informed decisions about your
+                    health
+                  </h2>
+                </div>
+                <button className="flex min-w-[84px] max-w-[480px] cursor-pointer items-center justify-center overflow-hidden rounded h-10 px-4 @[480px]:h-12 @[480px]:px-5  bg-[#80ED99] text-[#FFFFFF] text-sm font-bold leading-normal tracking-[0.015em] @[480px]:text-base @[480px]:font-bold @[480px]:leading-normal @[480px]:tracking-[0.015em]">
+                  <span className="truncate">Start your risk assessment</span>
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col gap-10 px-4 py-10 @container">
+            <h1 className="text-[#141414] tracking-light text-[32px] font-bold leading-tight @[480px]:text-4xl @[480px]:font-black @[480px]:leading-tight @[480px]:tracking-[-0.033em] max-w-[720px]">
+              How it works
+            </h1>
+          </div>
+          <div className="grid grid-cols-[repeat(auto-fit,minmax(158px,1fr))] gap-3 p-4 flex-wrap">
+            <div className="flex flex-col gap-3">
+              <div
+                className="w-full bg-center bg-no-repeat aspect-square bg-cover rounded"
+                style={{
+                  backgroundImage:
+                    'url("https://cdn.usegalileo.ai/sdxl10/d4501149-1dd9-415e-8dbf-65ceef1c8ddf.png")',
+                }}
+              />
+            </div>
+            <div className="flex flex-col gap-3">
+              <div
+                className="w-full bg-center bg-no-repeat aspect-square bg-cover rounded"
+                style={{
+                  backgroundImage:
+                    'url("https://cdn.usegalileo.ai/sdxl10/42510950-8fed-4a65-94fd-7ee24d1a428e.png")',
+                }}
+              />
+            </div>
+            <div className="flex flex-col gap-3">
+              <div
+                className="w-full bg-center bg-no-repeat aspect-square bg-cover rounded"
+                style={{
+                  backgroundImage:
+                    'url("https://cdn.usegalileo.ai/sdxl10/77a2414d-9f47-4074-b08d-b657c998fe1c.png")',
+                }}
+              />
+            </div>
+            <div className="flex flex-col gap-3">
+              <div
+                className="w-full bg-center bg-no-repeat aspect-square bg-cover rounded"
+                style={{
+                  backgroundImage:
+                    'url("https://cdn.usegalileo.ai/stability/78c2d39b-e69e-4ac0-a5d6-4a8b311ff810.png")',
+                }}
+              />
+            </div>
+            <div className="flex flex-col gap-3">
+              <div
+                className="w-full bg-center bg-no-repeat aspect-square bg-cover rounded"
+                style={{
+                  backgroundImage:
+                    'url("https://cdn.usegalileo.ai/stability/a8b74b4f-04e6-4385-8d4e-7b8f7e08f4e4.png")',
+                }}
+              />
+            </div>
+            <div className="flex flex-col gap-3">
+              <div
+                className="w-full bg-center bg-no-repeat aspect-square bg-cover rounded"
+                style={{
+                  backgroundImage:
+                    'url("https://cdn.usegalileo.ai/stability/f8dfab6a-1181-4f85-8a33-9549b3ff5752.png")',
+                }}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
