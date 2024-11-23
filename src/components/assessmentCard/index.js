74edb45cb49278de50bc9b95f15adac72246d38a
kiran <kiranugale239@gmail.com>
2024-11-23 22:50:31 +0530

    add assessment page card

---
+++ src/components/assessmentCard/index.js
@@ -0,0 +1,10 @@
+"use client";
+export default function AssessmentPageCard() {
+  return (
+    <>
+      <div className="mt-5 lg:p-24">
+        <h1>AssessmentPage </h1>
+      </div>
+    </>
+  );
+}
