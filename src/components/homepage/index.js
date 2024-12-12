fd27b30e6ef3032ffcc50905aedac59590221a3c
Ritesh <riteshvishwambhare2004@gmail.com>
2024-12-12 12:16:33 +0530

    ok

--- src/components/homepage/index.js
+++ src/components/homepage/index.js
@@ -5,8 +5,8 @@ import { useRouter } from "next/navigation";
 export default function HomePage() {
   return (
     <>
-      <div className="lg:px-0 w-full flex flex-col">
-        <div className="block lg:flex w-full" id="section1">
+      <div className="flex flex-col w-full lg:px-0">
+        <div className="block w-full lg:flex" id="section1">
           {/* <div class="hidden absolute left-[35.33%] right-[59.01%] top-[43.19%] bottom-[83.02%] ">
             <img src="heart.png" width={300}></img>
           </div> */}
@@ -77,7 +77,7 @@ export default function HomePage() {
         </div>
 
         {/*  Section  2*/}
-        <div className="block lg:flex mt-10 flex-col w-full" id="section2">
+        <div className="flex-col block w-full mt-10 lg:flex" id="section2">
           {/* top heading */}
           <div
             className="flex w-full mb-[30px] drop-shadow-none"
@@ -132,21 +132,197 @@ export default function HomePage() {
         </div>
 
         {/*  Section 3 */}
-        <div className="block lg:flex mt-10 flex-col w-full" id="section3">
+        <div
+          className="block absolute top-[2300px] lg:static lg:flex mt-10 flex-col w-full"
+          id="section3"
+        >
           <div
-            className="flex items-center justify-center w-full  mb-[30px] drop-shadow-none"
+            className="flex items-center justify-center w-full  lg:mb-[30px] drop-shadow-none"
             id="section3-topside"
             style={{ boxShadow: "none" }}
           >
-            <div className="flex flex-row gap-[10px] justify-center mx-[107px] text-wrap w-full font-exo text-[60px] font-bold leading-[72px] text-white">
-              <span className="bg-[#80ED99] bg-lime-500 pl-[30px] w-[155px] h-[80px]">
+            <div className="block lg:flex   gap-[5px] lg:gap-[10px] justify-center mx-[20px] lg:mx-[107px] text-wrap w-full font-exo text-[50px] lg:text-[60px] font-bold leading-[72px] text-white">
+              <span className="bg-[#80ED99] bg-lime-500 mx-[30px] w-[180px] p-2 lg:p-0 lg:mx-0 lg:pl-[30px] lg:w-[155px] h-[80px]">
                 {" "}
                 why
               </span>
               you should take it
             </div>
           </div>
-        </div>
+          <div className="block lg:flex w-full item-center gap-20 px-[70px]">
+            <div class="flex justify-center gap-5 flex-wrap">
+              <div class="w-[250px] bg-green-800 h-[200px] p-5  rounded-lg text-left shadow-md relative">
+                <div class="absolute w-[207px] h-[34px]  font-exo-2 font-semibold text-[28px] leading-[34px] text-white">
+                  Quick and easy
+                </div>
+                <div class="absolute w-[181px] h-[38px] top-[60px] font-exo-2 font-medium text-[16px] leading-[19px] text-white">
+                  Takes just 10-15 minutes to complete
+                </div>
+                <div className="absolute w-[30px] h-[30px]  left-[26px] top-[110px]   bg-transparent  ">
+                  <svg
+                    className=""
+                    width="50"
+                    height="50"
+                    viewBox="0 0 50 50"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <circle
+                      cx="25"
+                      cy="25"
+                      r="24"
+                      fill="white"
+                      stroke="#113D3C"
+                      strokeWidth="2"
+                    />
+                  </svg>
+                  <svg
+                    className="absolute top-[18px]  left-[16px]"
+                    width="14"
+                    height="14"
+                    viewBox="0 0 14 14"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M13.9134 1.58557C13.9131 1.18807 13.7551 0.806921 13.474 0.525845C13.193 0.244769 12.8118 0.0867525 12.4143 0.086505L4.4134 0.0843842C4.01545 0.084384 3.6338 0.24247 3.3524 0.523865C3.07101 0.805259 2.91292 1.18691 2.91292 1.58486C2.91292 1.98282 3.07101 2.36447 3.3524 2.64586C3.6338 2.92726 4.01545 3.08535 4.4134 3.08535L8.79251 3.08605L0.636038 11.2425C0.354733 11.5238 0.1967 11.9054 0.196699 12.3032C0.196698 12.701 0.354734 13.0825 0.636038 13.3638C0.917343 13.6452 1.29887 13.8032 1.6967 13.8032C2.09452 13.8032 2.47605 13.6452 2.75736 13.3638L10.9138 5.20737L10.9138 9.58578C10.9138 9.78282 10.9526 9.97794 11.0281 10.16C11.1035 10.342 11.214 10.5074 11.3533 10.6468C11.4926 10.7861 11.6581 10.8966 11.8401 10.972C12.0222 11.0474 12.2173 11.0863 12.4143 11.0863C12.6114 11.0863 12.8065 11.0474 12.9885 10.972C13.1706 10.8966 13.336 10.7861 13.4753 10.6468C13.6146 10.5074 13.7252 10.342 13.8006 10.16C13.876 9.97794 13.9148 9.78282 13.9148 9.58578L13.9134 1.58557Z"
+                      fill="#113D3C"
+                    />
+                  </svg>
+                </div>
+                <div class="absolute w-[108px] h-[117px] left-[131px] z-50 top-[80px] bg-[url('https://tinyurl.com/ms7tp28k')] bg-cover"></div>
+                <div class="absolute w-[250px] z-10 h-[40px] left-[0px] top-[169px] bg-[#EFBB59] rounded-b-[15px]"></div>
+              </div>
+
+              {/* box 2 */}
+              <div class="w-[250px] bg-[#406F6F] h-[200px] p-5  rounded-lg text-left shadow-md relative">
+                <div class="absolute w-[207px] h-[34px]  font-exo-2 font-semibold text-[28px] leading-[34px] text-white">
+                  Comprehensive
+                </div>
+                <div class="absolute w-[181px] h-[38px] top-[60px] font-exo-2 font-medium text-[16px] leading-[19px] text-white">
+                  Assesses your risk for 12 major diseases
+                </div>
+                <div className="absolute w-[30px] h-[30px]  left-[26px] top-[110px]   bg-transparent">
+                  <svg
+                    width="50"
+                    height="50"
+                    viewBox="0 0 50 50"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <circle
+                      cx="25"
+                      cy="25"
+                      r="24"
+                      fill="white"
+                      stroke="#113D3C"
+                      strokeWidth="2"
+                    />
+                  </svg>
+                  <svg
+                    className="absolute top-[18px] left-[16px]"
+                    width="14"
+                    height="14"
+                    viewBox="0 0 14 14"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M13.9134 1.58557C13.9131 1.18807 13.7551 0.806921 13.474 0.525845C13.193 0.244769 12.8118 0.0867525 12.4143 0.086505L4.4134 0.0843842C4.01545 0.084384 3.6338 0.24247 3.3524 0.523865C3.07101 0.805259 2.91292 1.18691 2.91292 1.58486C2.91292 1.98282 3.07101 2.36447 3.3524 2.64586C3.6338 2.92726 4.01545 3.08535 4.4134 3.08535L8.79251 3.08605L0.636038 11.2425C0.354733 11.5238 0.1967 11.9054 0.196699 12.3032C0.196698 12.701 0.354734 13.0825 0.636038 13.3638C0.917343 13.6452 1.29887 13.8032 1.6967 13.8032C2.09452 13.8032 2.47605 13.6452 2.75736 13.3638L10.9138 5.20737L10.9138 9.58578C10.9138 9.78282 10.9526 9.97794 11.0281 10.16C11.1035 10.342 11.214 10.5074 11.3533 10.6468C11.4926 10.7861 11.6581 10.8966 11.8401 10.972C12.0222 11.0474 12.2173 11.0863 12.4143 11.0863C12.6114 11.0863 12.8065 11.0474 12.9885 10.972C13.1706 10.8966 13.336 10.7861 13.4753 10.6468C13.6146 10.5074 13.7252 10.342 13.8006 10.16C13.876 9.97794 13.9148 9.78282 13.9148 9.58578L13.9134 1.58557Z"
+                      fill="#113D3C"
+                    />
+                  </svg>
+                </div>
+                <div class="absolute w-[108px] h-[137px] left-[111px] z-50 top-[80px] bg-[url('https://tinyurl.com/37szpwhm')] bg-cover"></div>
+                <div class="absolute w-[250px] z-10 h-[40px] left-[0px] top-[169px] bg-[#8FCBCD] rounded-b-[15px]"></div>
+              </div>
+              {/* box 3 */}
+              <div class="w-[260px] bg-[#A25900] h-[200px] p-5  rounded-lg text-left shadow-md relative">
+                <div class="absolute w-[207px] h-[34px]  font-exo-2 font-semibold text-[26px] leading-[34px] text-white">
+                  Personalized recommendations
+                </div>
+                <div class="absolute w-[181px] h-[38px] top-[80px] font-exo-2 font-medium text-[16px] leading-[19px] text-white">
+                  Based on your unique health profile
+                </div>
+                <div className="absolute w-[30px] h-[30px]  left-[26px] top-[120px]   bg-transparent">
+                  <svg
+                    width="50"
+                    height="50"
+                    viewBox="0 0 50 50"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <circle
+                      cx="25"
+                      cy="25"
+                      r="24"
+                      fill="white"
+                      stroke="#113D3C"
+                      strokeWidth="2"
+                    />
+                  </svg>
+                  <svg
+                    className="absolute top-[18px] left-[16px]"
+                    width="14"
+                    height="14"
+                    viewBox="0 0 14 14"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M13.9134 1.58557C13.9131 1.18807 13.7551 0.806921 13.474 0.525845C13.193 0.244769 12.8118 0.0867525 12.4143 0.086505L4.4134 0.0843842C4.01545 0.084384 3.6338 0.24247 3.3524 0.523865C3.07101 0.805259 2.91292 1.18691 2.91292 1.58486C2.91292 1.98282 3.07101 2.36447 3.3524 2.64586C3.6338 2.92726 4.01545 3.08535 4.4134 3.08535L8.79251 3.08605L0.636038 11.2425C0.354733 11.5238 0.1967 11.9054 0.196699 12.3032C0.196698 12.701 0.354734 13.0825 0.636038 13.3638C0.917343 13.6452 1.29887 13.8032 1.6967 13.8032C2.09452 13.8032 2.47605 13.6452 2.75736 13.3638L10.9138 5.20737L10.9138 9.58578C10.9138 9.78282 10.9526 9.97794 11.0281 10.16C11.1035 10.342 11.214 10.5074 11.3533 10.6468C11.4926 10.7861 11.6581 10.8966 11.8401 10.972C12.0222 11.0474 12.2173 11.0863 12.4143 11.0863C12.6114 11.0863 12.8065 11.0474 12.9885 10.972C13.1706 10.8966 13.336 10.7861 13.4753 10.6468C13.6146 10.5074 13.7252 10.342 13.8006 10.16C13.876 9.97794 13.9148 9.78282 13.9148 9.58578L13.9134 1.58557Z"
+                      fill="#113D3C"
+                    />
+                  </svg>
+                </div>
+                <div class="absolute w-[108px] h-[107px] left-[131px] z-50 top-[90px] bg-[url('https://tinyurl.com/2ch2pwsv')] bg-cover"></div>
+                <div class="absolute w-[260px] z-10 h-[40px] left-[0px] top-[169px] bg-[#B27122] rounded-b-[15px]"></div>
+              </div>
+              {/* box 4 */}
+              <div class="w-[250px] bg-[#BFCE00] h-[200px] p-5  rounded-lg text-left shadow-md relative">
+                <div class="absolute w-[207px] h-[34px]  font-exo-2 font-semibold text-[28px] leading-[34px] text-white">
+                  Track your progress
+                </div>
+                <div class="absolute w-[181px] h-[38px] top-[83px] font-exo-2 font-medium text-[16px] leading-[19px] text-white">
+                  Follow up assessments to monitor changes
+                </div>
+                <div className="absolute w-[20px] h-[20px]  left-[26px] top-[120px]   bg-transparent">
+                  <svg
+                    width="50"
+                    height="50"
+                    viewBox="0 0 50 50"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <circle
+                      cx="25"
+                      cy="25"
+                      r="24"
+                      fill="white"
+                      stroke="#113D3C"
+                      strokeWidth="2"
+                    />
+                  </svg>
+                  <svg
+                    className="absolute top-[18px] left-[16px]"
+                    width="14"
+                    height="14"
+                    viewBox="0 0 14 14"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M13.9134 1.58557C13.9131 1.18807 13.7551 0.806921 13.474 0.525845C13.193 0.244769 12.8118 0.0867525 12.4143 0.086505L4.4134 0.0843842C4.01545 0.084384 3.6338 0.24247 3.3524 0.523865C3.07101 0.805259 2.91292 1.18691 2.91292 1.58486C2.91292 1.98282 3.07101 2.36447 3.3524 2.64586C3.6338 2.92726 4.01545 3.08535 4.4134 3.08535L8.79251 3.08605L0.636038 11.2425C0.354733 11.5238 0.1967 11.9054 0.196699 12.3032C0.196698 12.701 0.354734 13.0825 0.636038 13.3638C0.917343 13.6452 1.29887 13.8032 1.6967 13.8032C2.09452 13.8032 2.47605 13.6452 2.75736 13.3638L10.9138 5.20737L10.9138 9.58578C10.9138 9.78282 10.9526 9.97794 11.0281 10.16C11.1035 10.342 11.214 10.5074 11.3533 10.6468C11.4926 10.7861 11.6581 10.8966 11.8401 10.972C12.0222 11.0474 12.2173 11.0863 12.4143 11.0863C12.6114 11.0863 12.8065 11.0474 12.9885 10.972C13.1706 10.8966 13.336 10.7861 13.4753 10.6468C13.6146 10.5074 13.7252 10.342 13.8006 10.16C13.876 9.97794 13.9148 9.78282 13.9148 9.58578L13.9134 1.58557Z"
+                      fill="#113D3C"
+                    />
+                  </svg>
+                </div>
+                <div class="absolute w-[108px] h-[117px] left-[131px] z-50 top-[100px] bg-[url('https://tinyurl.com/3dwakyhs')] bg-cover"></div>
+                <div class="absolute w-[250px] z-10 h-[40px] left-[0px] top-[169px] bg-[#E8F44A] rounded-b-[15px]"></div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
